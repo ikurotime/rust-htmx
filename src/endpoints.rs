a0523f46cb42a82443130893331f9da5f921cdb6
David Huertas <davidhuertasortiz@gmail.com>
2023-11-27 22:53:39 +0100

    first commit: Web Server & Tailwind

---
+++ src/endpoints.rs
@@ -0,0 +1,65 @@
+use axum::{Json, response::{Html, IntoResponse, Response}, http::StatusCode};
+use serde::{Serialize, Deserialize};
+
+use askama::Template;
+// PAGE ONE
+pub async fn get_index() -> impl IntoResponse {
+    let template = HelloTemplate {};
+    HtmlTemplate(template)
+}
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate;
+// PAGE TWO
+pub async fn get_page_two() -> impl IntoResponse {
+    let template = AnotherPage{};
+    HtmlTemplate(template)
+}
+
+#[derive(Template)]
+#[template(path = "another_page.html")]
+struct AnotherPage;
+
+
+
+struct HtmlTemplate<T>(T);
+
+impl<T> IntoResponse for HtmlTemplate<T>
+where
+    T: Template,
+{
+    fn into_response(self) -> Response {
+        // Attempt to render the template with askama
+        match self.0.render() {
+            // If we're able to successfully parse and aggregate the template, serve it
+            Ok(html) => Html(html).into_response(),
+            // If we're not, return an error or some bit of fallback HTML
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {}", err),
+            )
+                .into_response(),
+        }
+    }
+}
+
+pub async fn post_data(Json(payload):Json<CreateMessage>) -> Json<Message>{
+    let message = Message{
+        id: 1,
+        message: payload.message.to_string(),
+    };
+    Json(message)
+}
+
+#[derive(Serialize)]
+pub struct Message {
+    id: i32,
+    message: String,
+}
+
+#[derive(Deserialize)]
+pub struct CreateMessage {
+    message: String,
+}
+
---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use tower_http::services::ServeDir;
+use tracing::info;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use axum::{Router,routing::{get, post} };
+mod endpoints;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()>{
+         tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "with_axum_htmx_askama=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+ 
+    info!("hello, web server!");   
+let assets_path = std::env::current_dir().unwrap();
+
+    let app = Router::new()
+        .route("/", get(endpoints::get_index))
+        .route("/page-two", get(endpoints::get_page_two))
+        .route("/create", post(endpoints::post_data)).nest_service(
+        "/assets",
+        ServeDir::new(format!("{}/assets", assets_path.to_str().unwrap())),
+    );
+
+
+        let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+ info!("router initialized, now listening on port {}", 3000);
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
+}
+
